dfeac4c88d0a10fb01d569b595ecbf8d6234f829
GodwinEgo <godwinehile04@gmail.com>
2023-04-20 15:47:16 +0100

    login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,22 +2,28 @@ import { Typography, createTheme } from '@mui/material';
 import React from 'react'
 import { Link } from 'react-router-dom';
 
+
+
+function Copyright ( props )
+{
+  return (
+    <Typography variant='body2 ' color='text.secondary' align='center' { ...props }>
+      { 'Copyright @ ' }
+      <Link color='inherit' href='#'>Nbuddy</Link>{ ' ' }
+      { new Date().getFullYear() }
+      ['.']
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+
 const Login = () =>
 {
 
-  function Copyright ( props )
-  {
-    return (
-      <Typography variant='body2 ' color='text.secondary' align='center' { ...props }>
-        { 'Copyright @ ' }
-        <Link color='inherit' href='#'>Nbuddy</Link>{ ' ' }
-        { new Date().getFullYear() }
-        ['.']
-      </Typography>
-    );
-  }
+  const handleSubmit
 
-  const theme = createTheme();
 
 }
 
